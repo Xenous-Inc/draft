3300f0d71c51c73b187daf673672b24e563bc7a6
x3noku <artem.kaikov@yandex.ru>
2021-05-07 14:31:26 +0600

    Initial commit

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: ["inline-dotenv"],
+    env: {
+      production: {
+        plugins: [["inline-dotenv", {
+          path: '.env.prod'
+        }]]
+      },
+      development: {
+        plugins: [["inline-dotenv", {
+          path: '.env.dev'
+        }]]
+      },
+    }
+  };
+};
