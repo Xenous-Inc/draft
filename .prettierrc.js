ae09edd8a6dd7b5e79df86d26c198b9db0d139ad
labamnus <labamnus@gmail.com>
2021-05-11 07:33:09 +0600

    Add eslint commit

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    jsxSingleQuote: true,
+    printWidth: 80,
+    semi: true,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: 'all',
+    quoteProps: 'consistent',
+    arrowParens: 'avoid',
+};
--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,29 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: ["inline-dotenv", 'react-native-reanimated/plugin'],
-    env: {
-      production: {
-        plugins: [["inline-dotenv", {
-          path: '.env.prod'
-        }]]
-      },
-      development: {
-        plugins: [["inline-dotenv", {
-          path: '.env.dev'
-        }]]
-      },
-    }
-  };
+module.exports = function (api) {
+    api.cache(true);
+    return {
+        presets: ['babel-preset-expo'],
+        plugins: ['inline-dotenv', 'react-native-reanimated/plugin'],
+        env: {
+            production: {
+                plugins: [
+                    [
+                        'inline-dotenv',
+                        {
+                            path: '.env.prod',
+                        },
+                    ],
+                ],
+            },
+            development: {
+                plugins: [
+                    [
+                        'inline-dotenv',
+                        {
+                            path: '.env.dev',
+                        },
+                    ],
+                ],
+            },
+        },
+    };
 };
